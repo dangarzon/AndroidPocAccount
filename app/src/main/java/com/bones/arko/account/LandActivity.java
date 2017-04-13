f360e989ed521f163fa4781973cd4b67f9fdbb6a
Daniel Garzon <daniel.garzon.sanchez@gmail.com>
2017-04-13 23:40:20 +0200

    Redirect from Land to Login

--- app/src/main/java/com/bones/arko/account/LandActivity.java
+++ app/src/main/java/com/bones/arko/account/LandActivity.java
@@ -1,9 +1,13 @@
 package com.bones.arko.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class LandActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     /**
      * {@inheritDoc}
@@ -12,6 +16,17 @@ public class LandActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land);
+
+        // Sleep a little
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Log.d(TAG, "Unexpected sleep error!!");
+        }
+
+        // Go to Login
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 }
