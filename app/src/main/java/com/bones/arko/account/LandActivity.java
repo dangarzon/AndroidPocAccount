644382727fc632513c77a978f6bda47aea8f257e
Daniel Garzon <daniel.garzon.sanchez@gmail.com>
2017-04-14 03:41:39 +0200

    Redirect to Main from Land if Account already exists

--- app/src/main/java/com/bones/arko/account/LandActivity.java
+++ app/src/main/java/com/bones/arko/account/LandActivity.java
@@ -1,7 +1,10 @@
 package com.bones.arko.account;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -24,8 +27,39 @@ public class LandActivity extends AppCompatActivity {
             Log.d(TAG, "Unexpected sleep error!!");
         }
 
-        // Go to Login
-        Intent intent = new Intent(this, LoginActivity.class);
+        if (hasAccount()) {
+            goActivity(MainActivity.class);
+        } else {
+            goActivity(LoginActivity.class);
+        }
+    }
+
+    /**
+     * Check if User has Account
+     *
+     * @return true if the Account exists
+     */
+    @NonNull
+    private Boolean hasAccount() {
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccountsByType(getString(R.string.account_type));
+
+        Boolean match = false;
+        for (Account account : accounts) {
+            if (account.name.equals(getString(R.string.account_name))) {
+                match = true;
+            }
+        }
+        return match;
+    }
+
+    /**
+     * Go to Activity
+     *
+     * @param aClass class of the Activity to go to
+     */
+    private void goActivity(Class aClass) {
+        Intent intent = new Intent(this, aClass);
         startActivity(intent);
     }
 
