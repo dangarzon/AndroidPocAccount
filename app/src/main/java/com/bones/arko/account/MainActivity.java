de91dcc03fdf6e84bf61e6c464bb095acc2464c9
Daniel Garzon <daniel.garzon.sanchez@gmail.com>
2017-04-14 18:48:48 +0200

    Add button to call url with token

--- app/src/main/java/com/bones/arko/account/MainActivity.java
+++ app/src/main/java/com/bones/arko/account/MainActivity.java
@@ -7,7 +7,9 @@ import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +18,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private AccountManager accountManager;
 
     private View mProgressView;
+
+    private TextView mResponseTextView;
 
     /**
      * {@inheritDoc}
@@ -39,6 +46,23 @@ public class MainActivity extends AppCompatActivity {
         showUserInfo();
 
         mProgressView = findViewById(R.id.progress_bar);
+        mResponseTextView = (TextView) findViewById(R.id.response_text_view);
+
+        // Bind token ok button to action
+        Button mTokenOkButton = (Button) findViewById(R.id.call_token_ok_button);
+        mTokenOkButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setEnabled(false);
+
+                String token = getToken();
+
+                processCall(
+                    getString(R.string.uri_secured),
+                    token
+                );
+            }
+        });
 
         // Bind logout button to action
         Button mLogoutButton = (Button) findViewById(R.id.logout_button);
@@ -52,6 +76,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Call url with given token
+     *
+     * @param uri uri to call
+     * @param token authorization bearer token
+     * @return response status code
+     */
+    private Integer doCall(String uri, String token) {
+        Integer statusCode = null;
+
+        try {
+            URL url = new URL(uri);
+
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty(
+                "Authorization",
+                "Bearer " + token
+            );
+
+            statusCode = connection.getResponseCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return statusCode;
+    }
+
+    /**
+     * Get User Account
+     *
+     * @return user account
+     */
     private Account getAccount() {
         Account account = null;
         Account[] accounts = accountManager.getAccountsByType(getString(R.string.account_type));
@@ -63,6 +121,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return account;
+    }
+
+    /**
+     * Get user token
+     *
+     * @return token
+     */
+    @Nullable
+    private String getToken() {
+        Account account = getAccount();
+
+        return (null != account)
+            ? accountManager.getUserData(account, "token")
+            : null;
     }
 
     /**
@@ -99,6 +171,28 @@ public class MainActivity extends AppCompatActivity {
                 null
             );
         }
+    }
+
+    /**
+     * Do call and update Response TextView
+     *
+     * @param url url to call
+     * @param token auth token
+     */
+    private void processCall(final String url, final String token) {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                final Integer status = doCall(url, token);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mResponseTextView.setText(status.toString());
+                    }
+                });
+            }
+        });
     }
 
     /**
